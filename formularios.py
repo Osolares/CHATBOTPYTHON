fe0b657952922d94d75ad1ae05b0186eb7bf70fd
Osolares <osolares1116503@gmail.com>
2025-04-10 23:05:42 -0600

    VariablesEntorno

--- formularios.py
+++ formularios.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
 from models import UserSession, ProductModel, db
-from app import generar_menu_principal
 import time
 
 def formulario_motor(number):
@@ -212,8 +211,38 @@ def cancelar_flujo(number):
                 "body": "🚪 Formulario cancelado. Has salido del formulario actual. ¿Qué deseas hacer ahora?"
             }
         },
-        generar_menu_principal(number)
-
+        {
+            "messaging_product": "whatsapp",
+            "to": number,
+            "type": "interactive",
+            "interactive": {
+                "type": "list",
+                "body": {
+                    "text": "🌐 Visita nuestro sitio web www.intermotores.com para más información.\n\n1️⃣ ⚙ Motores\n2️⃣ 🛞 Repuestos\n3️⃣ 📍 Ubicación\n4️⃣ 🕜 Horario de Atención\n5️⃣ ☎ Contacto\n6️⃣  💳 Formas de pago y números de cuenta\n7️⃣ ⏳ Esperar para ser atendido por nuestro personal\n8️⃣ 🚛 Opciones de envío\n0️⃣ 🔙 Regresar al Menú \n\n📌 *Escribe el número #️⃣ de tu elección.*"
+                },
+                "footer": {
+                    "text": ""
+                },
+                "action": {
+                    "button": "Ver Menú",
+                    "sections": [
+                        {
+                            "title": "Opciones Principales",
+                            "rows": [
+                                {"id": "1", "title": "1️⃣ ⚙Motores", "description": "Cotizar Motores"},
+                                {"id": "2", "title": "2️⃣ 🛞Repuestos", "description": "Cotizar Repuestos"},
+                                {"id": "3", "title": "3️⃣ 📍Ubicación", "description": "Dónde estamos ubicados"},
+                                {"id": "4", "title": "4️⃣ 🕜Horario", "description": "Horario de atención"},
+                                {"id": "5", "title": "5️⃣ ☎Contacto", "description": "Contáctanos"},
+                                {"id": "6", "title": "6️⃣ 💳Cuentas y Pagos", "description": "Cuentas de banco y formas de pago"},
+                                {"id": "7", "title": "7️⃣ ⏳Hablar con personal", "description": "Esperar para ser atendido por nuestro personal"},
+                                {"id": "8", "title": "8️⃣ 🚛Envíos", "description": "Opciones de envío"}
+                                ]
+                        }
+                    ]
+                }
+            }
+        }
     ]
 
 def actualizar_interaccion(number):
